57895ca87ce6b70eac0c6e706c3354add77d1399
root <p.amoako2002@gmail.com>
2022-10-03 14:05:50 -0700

    it is getting better

---
+++ fifth-line.py
@@ -0,0 +1,10 @@
+#this is the code you all have being waiting for :)
+print("MY CODES")
+n = int(input("the number: "))
+m = int(input("the number: "))
+p = m+n
+print(p)
+if p%2==0:
+    print('it is an even number')
+else:
+    print("it is an odd number")
