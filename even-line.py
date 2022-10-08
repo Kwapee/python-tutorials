e4bd1244b7528e49b314e41daba42e1c7fdc448b
root <p.amoako2002@gmail.com>
2022-10-07 17:01:03 -0700

    even

---
+++ even-line.py
@@ -0,0 +1,10 @@
+# This is for me
+mv = int(input('please the number: '))
+nv = int(input('Please the number: '))
+p = mv*nv
+print(p)
+if p%2==0:
+    print('it is an even number')
+else:
+    print('it is an odd number')
+print("DONE")  
